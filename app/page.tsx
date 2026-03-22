478391e4af51c05c0c9b25ccfd6b87286481e441
sjoanna2403-alt <sjoanna2403@gmail.com>
2026-03-22 20:00:32 +0000

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding: "40px", fontFamily: "Arial"}}>
+      <h1>With Joanna Beauty</h1>
+      <p>Strona działa 🎉</p>
+    </main>
+  );
+}
