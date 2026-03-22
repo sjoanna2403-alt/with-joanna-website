9f6356ffaa48c708f0d415217bd216ac98e59262
sjoanna2403-alt <sjoanna2403@gmail.com>
2026-03-22 20:18:30 +0000

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <main style={{padding: "40px", fontFamily: "Arial"}}>
-      <h1>With Joanna Beauty</h1>
-      <p>Strona działa 🎉</p>
-    </main>
-  );
-}
